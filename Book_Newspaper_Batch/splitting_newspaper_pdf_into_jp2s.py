dde65ea687de338653ed824f3d32e84214bd4891
GarrettArm <garrett.w.armstrong@gmail.com>
2018-01-09 20:02:59 +0000

    more organized, moves parent pdf

--- Book_Newspaper_Batch/splitting_newspaper_pdf_into_jp2s.py
+++ Book_Newspaper_Batch/splitting_newspaper_pdf_into_jp2s.py
@@ -6,17 +6,18 @@ import shutil
 import sys
 
 
-def split_pdf_to_jp2s(pdf_file):
+def split_pdf_to_jp2s(pdf_file, output_root):
     root, filename = os.path.split(pdf_file)
-    dest_root = os.path.join(root, 'Jp2BookConverted', os.path.splitext(filename)[0])
+    dest_root = os.path.join(output_root, os.path.splitext(filename)[0])
     os.makedirs(dest_root, exist_ok=True)
-    print(root, filename)
     subprocess.call(['convert',
                      '-density',
                      '300',
                      os.path.join(root, filename),
                      '-quality',
                      '75',
+                     '-colorspace',
+                     'RGB',
                      os.path.join(dest_root, '%03d.jp2'),
                      ])
 
@@ -26,42 +27,75 @@ def move_jp2s_to_subfolders(filepath):
     prefix, extension = os.path.splitext(filename)
     prefix_plus_one = str(int(prefix) + 1)
     dest_filepath = os.path.join(root, prefix_plus_one)
-    os.makedirs(dest_filepath)
-    shutil.move(filepath, dest_filepath)
-    shutil.move(os.path.join(dest_filepath, filename), os.path.join(dest_filepath, 'OBJ.jp2'))
+    os.makedirs(dest_filepath, exist_ok=True)
+    shutil.move(filepath, os.path.join(dest_filepath, 'OBJ.jp2'))
 
 
-def move_mods_files(mods_file):
-    root, filename = os.path.split(mods_file)
-    dest_root = os.path.join(root, 'Jp2BookConverted', os.path.splitext(filename)[0])
+def move_mods_files(source_filepath, output_root):
+    root, filename = os.path.split(source_filepath)
+    dest_root = os.path.join(output_root, os.path.splitext(filename)[0])
     os.makedirs(dest_root, exist_ok=True)
-    shutil.copy2(mods_file, os.path.join(dest_root, 'MODS.xml'))
+    dest_filepath = os.path.join(dest_root, 'MODS.xml')
+    print(source_filepath, dest_filepath)
+    shutil.copy2(source_filepath, dest_filepath)
 
 
-def find_all_filetype(directory, filetype):
-    all_this_type = [os.path.join(root, file)
-                     for root, dirs, files in os.walk(directory)
-                     for file in files
-                     if os.path.splitext(file)[1] == filetype]
-    return all_this_type
+def find_all_jp2s(output_root):
+    all_output_jp2s = []
+    for parent_folder in os.listdir(output_root):
+        parent_path = os.path.join(output_root, parent_folder)
+        if not os.path.isdir(parent_path):
+            continue
+        for file in os.listdir(parent_path):
+            child_filepath = os.path.join(parent_path, file)
+            if not os.path.isfile(child_filepath):
+                continue
+            if os.path.splitext(child_filepath)[1] == '.jp2':
+                all_output_jp2s.append(child_filepath)
+    return all_output_jp2s
+
+
+def move_pdfs_to_subfolders(source_filepath, output_root):
+    root, filename = os.path.split(source_filepath)
+    dest_root = os.path.join(output_root, os.path.splitext(filename)[0])
+    os.makedirs(dest_root, exist_ok=True)
+    dest_filepath = os.path.join(dest_root, 'PDF.pdf')
+    print(source_filepath, dest_filepath)
+    shutil.copy2(source_filepath, dest_filepath)
 
 
 if __name__ == '__main__':
 
     try:
-        source_dir = sys.argv[1]
+        source_root = sys.argv[1]
     except IndexError:
         print('\nChange to: "python splitting_newspaper_pdf_into_jp2.py $path/to/source_pdf_folder"\n')
         quit()
-    all_pdf_files = find_all_filetype(source_dir, '.pdf')
-    all_mods_files = find_all_filetype(source_dir, '.xml')
-    all_jp2_files = find_all_filetype(source_dir, '.jp2')
 
-    for filepath in all_pdf_files:
-        split_pdf_to_jp2s(filepath)
+    source_root = os.path.realpath(source_root)
+    parent_root, source_folder = os.path.split(source_root)
+    output_root = os.path.join(parent_root, '{}-to-book'.format(source_folder))
+    os.makedirs(output_root, exist_ok=True)
 
-    for filepath in all_mods_files:
-        move_mods_files(filepath)
+    all_source_pdfs = [os.path.join(source_root, i)
+                       for i in os.listdir(source_root)
+                       if os.path.splitext(i)[1] == '.pdf']
 
-    for filepath in all_jp2_files:
+    for filepath in sorted(all_source_pdfs):
+        split_pdf_to_jp2s(filepath, output_root)
+
+    all_source_mods = [os.path.join(source_root, i)
+                       for i in os.listdir(source_root)
+                       if os.path.splitext(i)[1] == '.xml']
+
+    for filepath in sorted(all_source_mods):
+        move_mods_files(filepath, output_root)
+
+    all_output_jp2s = find_all_jp2s(output_root)
+
+    for filepath in sorted(all_output_jp2s):
         move_jp2s_to_subfolders(filepath)
+
+    for filepath in sorted(all_source_pdfs):
+        print(filepath)
+        move_pdfs_to_subfolders(filepath, output_root)
